738745aa4b68537c6a31cfecda5c96bc7020d4a4
nehaldamor <nehaldamor77@gmail.com>
2025-02-20 21:04:27 +0530

    Initial commit

---
+++ FRONTEND/src/components/CaptainDetails.jsx
@@ -0,0 +1,43 @@
+
+import React, { useContext } from 'react'
+import { CaptainDataContext } from '../context/CapatainContext'
+
+const CaptainDetails = () => {
+
+    const { captain } = useContext(CaptainDataContext)
+
+    return (
+        <div>
+            <div className='flex items-center justify-between bg-green-400 w-full p-2 rounded-lg'>
+                <div className='flex items-center justify-start gap-3'>
+                    <img className='h-20 w-20 rounded-full object-cover' src="./src/assets/Rider.jpeg" alt="" />
+                    <h4 className='text-lg font-medium capitalize'>{captain.fullname.firstname + " " + captain.fullname.lastname}</h4>
+                </div>
+                <div>
+                    <h4 className='text-xl font-semibold'>₹295.20</h4>
+                    <p className='text-sm text-gray-600'>Earned</p>
+                </div>
+            </div>
+            <div className='flex p-3 mt-8 bg-green-300 rounded-xl justify-center gap-5 items-start'>
+                <div className='text-center'>
+                    <i className="text-3xl mb-2 font-thin ri-timer-2-line"></i>
+                    <h5 className='text-lg font-medium'>10.2</h5>
+                    <p className='text-sm text-gray-600'>Hours Online</p>
+                </div>
+                <div className='text-center'>
+                    <i className="text-3xl mb-2 font-thin ri-speed-up-line"></i>
+                    <h5 className='text-lg font-medium'>10.2</h5>
+                    <p className='text-sm text-gray-600'>Hours Online</p>
+                </div>
+                <div className='text-center'>
+                    <i className="text-3xl mb-2 font-thin ri-booklet-line"></i>
+                    <h5 className='text-lg font-medium'>10.2</h5>
+                    <p className='text-sm text-gray-600'>Hours Online</p>
+                </div>
+
+            </div>
+        </div>
+    )
+}
+
+export default CaptainDetails

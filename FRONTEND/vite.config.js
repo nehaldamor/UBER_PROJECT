bb465d01c5d6cef1c74d15bb36c6e2fd4444d90e
nehaldamor <nehaldamor77@gmail.com>
2025-02-21 10:40:53 +0530

    Updated backend with latest changes

--- FRONTEND/vite.config.js
+++ FRONTEND/vite.config.js
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://uber-frontend-5x0l.onrender.com', // Your backend URL
+        changeOrigin: true,
+        secure: false,
+        rewrite: (path) => path.replace(/^\/api/, '') // Remove /api prefix
+      }
+    }
+  }
 })

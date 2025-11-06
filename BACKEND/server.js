91d71266a215ac69b9d126f79eaba0c10f4142bb
nehaldamor <168215150+nehaldamor@users.noreply.github.com>
2025-11-07 00:53:26 +0530

    Update server.js

--- BACKEND/server.js
+++ BACKEND/server.js
@@ -1,11 +1,11 @@
 const http = require('http');
 const app = require('./app');
-const cors=require('cors');
+
 const { initializeSocket } = require('./socket');
 const port = process.env.PORT || 3000;
 
 
-app.use(cors());
+
 const server = http.createServer(app);
 
 initializeSocket(server);

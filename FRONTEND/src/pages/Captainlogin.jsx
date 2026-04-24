ce579aa90ec5e02416ecd2714429c73ad8410911
nehaldamor <168215150+nehaldamor@users.noreply.github.com>
2026-04-24 14:05:46 +0530

    Update logo image source in Captainlogin component

--- FRONTEND/src/pages/Captainlogin.jsx
+++ FRONTEND/src/pages/Captainlogin.jsx
@@ -38,7 +38,7 @@ const Captainlogin = () => {
   return (
     <div className='p-7 h-screen flex flex-col justify-between'>
       <div>
-        <img className='w-20 mb-3' src="https://www.svgrepo.com/show/505031/uber-driver.svg" alt="" />
+        <img className='w-20 mb-3' src="https://cdn5.f-cdn.com/contestentries/2317795/51872027/651b04f8d812b_thumb900.jpg" alt="" />
 
         <form onSubmit={(e) => {
           submitHandler(e)
